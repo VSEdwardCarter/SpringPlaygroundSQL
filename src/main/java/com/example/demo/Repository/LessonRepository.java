bdb5b90fe80fc7166468f48de373202bfb3f5703
VSEdwardCarter <edward.j.carter2@gamil.com>
2021-09-23 13:47:40 -0500

    updated location local

---
+++ src/main/java/com/example/demo/Repository/LessonRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Model.Lesson;
+import org.springframework.data.repository.CrudRepository;
+
+public class LessonRepository extends CrudRepository<Lesson, Long> {
+}
