e4acdac77a90f8e4d61efdb73805873ff3928143
VSEdwardCarter <edward.j.carter2@gamil.com>
2021-09-23 15:15:21 -0500

    Chris and Carter's Final Solution

--- src/main/java/com/example/demo/Controller/LessonController.java
+++ src/main/java/com/example/demo/Controller/LessonController.java
@@ -4,6 +4,8 @@ import com.example.demo.Model.Lesson;
 import com.example.demo.Repository.LessonRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 
 @RestController
 @RequestMapping("/lessons")
@@ -14,13 +16,35 @@ public class LessonController {
         this.repository = repository;
     }
 
+    // Read
     @GetMapping("")
-    public Iterable<Lesson> all() {
-        return this.repository.findAll();
+    public Iterable<Lesson> all() { return this.repository.findAll(); }
+
+    @GetMapping("/")
+    public Lesson singleRecord(@PathVariable Long id){
+        return this.repository.findById(id).get();
     }
 
+    // Create
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
         return this.repository.save(lesson);
     }
+    //Update
+    @PatchMapping("/{id}")
+    public Lesson updateLessonTitle(@PathVariable Long id, @RequestBody Lesson lesson){
+        Lesson bob = this.repository.findById(id).get();
+        lesson.setId(id);
+        lesson.setDeliveredOn(bob.getDeliveredOn());
+
+        return this.repository.save(lesson);
+    }
+    //Delete
+    @DeleteMapping("/{id}")
+    public void deleteLesson(@PathVariable Long id){
+        this.repository.deleteById(id);
+
+    }
+
+
 }
--- src/main/java/com/example/demo/Repository/LessonRepository.java
+++ src/main/java/com/example/demo/Repository/LessonRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.Repository;
 import com.example.demo.Model.Lesson;
 import org.springframework.data.repository.CrudRepository;
 
-public class LessonRepository extends CrudRepository<Lesson, Long> {
+public interface LessonRepository extends CrudRepository<Lesson, Long> {
 }
